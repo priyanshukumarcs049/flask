04595cbe0ed4ba596a5ce2be29ee267de330914f
priyanshukumarcs049 <31381774+priyanshukumarcs049@users.noreply.github.com>
2018-10-16 12:42:49 +0530

    email_support and goggle authentication

---
+++ email_support/forms.py
@@ -0,0 +1,5 @@
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
